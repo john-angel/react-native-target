75f8092bf085dd82da12795d9e3693457ff9a435
John Angel <j_angeldiaz@hotmail.com>
2024-01-18 15:41:30 -0500

    style: add styles to TextInput component in StartGameScreen

--- screens/startGameScreen.js
+++ screens/startGameScreen.js
@@ -4,7 +4,12 @@ import PrimaryButton from "../components/primaryButton";
 export default function StartGameScreen(){
     return (
         <View style={ styles.inputContainer }>
-            <TextInput style={ styles.numberInput } maxLength={ 2 }/>
+            <TextInput 
+                style={ styles.numberInput }
+                maxLength={ 2 }
+                keyboardType="number-pad"
+                autoCapitalize="none"
+            />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
